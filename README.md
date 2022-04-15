ad3f6f49ad45d0a9e2ef943744c46c4933a68a75
Jarema Antosz <jarema.antosz@wp.pl>
2022-04-15 18:39:08 +0200

    Azure deploy

---
+++ README.md
@@ -0,0 +1,18 @@
+azure-webapp-maven-plugin needs subscriptionId
+
+Plugin setup:  mvn com.microsoft.azure:azure-webapp-maven-plugin:2.2.0:config
+It will write config to pom.xml
+
+F1 is Free Plan
+
+Deploy: mvn package azure-webapp:deploy
+It will create App Service Web App and deploy jar file to it
+
+
+Accessing logs in Azure CLI:
+az webapp log config --name <app-name> --resource-group <resource-group-name> --docker-container-logging filesystem
+az webapp log tail --name <app-name> --resource-group <resource-group-name>
+
+
+Clean up:
+az group delete --name <resource-group-name> --yes
--- src/main/java/com/jantosz/azuredemo/controller/DemoController.java
+++ src/main/java/com/jantosz/azuredemo/controller/DemoController.java
@@ -1,13 +1,24 @@
 package com.jantosz.azuredemo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    Logger LOGGER = LoggerFactory.getLogger(DemoController.class);
+
+    @GetMapping("/")
+    public String root() {
+        LOGGER.warn("accessing demo controller / path");
+        return "Hello from demo app root";
+    }
+
     @GetMapping("/demo")
     public String demo() {
+        LOGGER.warn("accessing demo controller /demo path");
         return "Hello from demo app";
     }
 }
